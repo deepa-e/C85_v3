581e2e12cb9afdf66104d0651cb0b7ce07c1bd21
deepa-e <72617958+deepa-e@users.noreply.github.com>
2021-11-08 17:34:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7CdeXGfIFgDEzZDXGFJgLekQrW53ttso",
+    authDomain: "story-telling-app-ed52c.firebaseapp.com",
+    databaseURL: "https://story-telling-app-ed52c.firebaseio.com",
+    projectId: "story-telling-app-ed52c",
+    storageBucket: "story-telling-app-ed52c.appspot.com",
+    messagingSenderId: "985298444646",
+    appId: "1:985298444646:web:52ac07a933cbecf4e768a7"
+  };
